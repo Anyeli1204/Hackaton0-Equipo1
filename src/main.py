fc6bc56821e06be400e4d6c2685522dabf2e2024
Luis Cahuana <luis.cahuana@utec.edu.pe>
2025-04-15 17:46:17 -0500

    Implementacion de Suma

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
-def calculate() -> float:
-    pass
+def calculate(string) -> float:
+    #cambia simbolo
+
+    primer = ""
+    segundo = ""
+
+    for i in string:
+        if (i == " "):
+            break
+        primer += i
+
+    for i in string[::-1]:
+        if (i == " "):
+            break
+        segundo += i
+
+    segundo = segundo[::-1]
+    first = int(primer)
+    second = int(segundo)
+
+    return first cambia signo second
